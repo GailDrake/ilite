680fbfddf43090de65fe0ac11dd4cd2a0d474a62
samueltoureau <samtour03@gmail.com>
2016-01-14 18:52:55 -0500

    Updated file type to php

--- index.php
+++ index.php
@@ -3,12 +3,16 @@
 <head lang="en">
     <meta charset="UTF-8">
     <title>iLite · US</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
 
 <?php  include("header.php");  ?>
+<?php  include("carousel.php");  ?>
 
-
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src='/js/min/ilite.min.js'></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </body>
 </html>
