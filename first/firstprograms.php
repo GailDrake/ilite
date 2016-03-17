1ed3d96dc57c7b09f4c7f2ab54f1d11a69db2c16
annapNC17 <n.c.annap@gmail.com>
2016-03-17 18:21:18 -0400

    Footer Includes

--- first/firstprograms.php
+++ first/firstprograms.php
@@ -77,49 +77,8 @@
 </div>
 </div><!-- end section -->
 
-<div id="wrap7">
-  <div id="footer-map">  </div>
 
-<div class="featured_section82 three">
-    <div class="ovfull_container">
-
-        <div class="box animate" data-anim-type="fadeInRight" data-anim-delay="200">
-            <a href="mailto:team1885@gmail.com?Subject=ILITE%ROBOTICS:" target="_top"><i class="fa fa-envelope-o"></i></a>
-            <a href="http://maps.google.com/maps/place/Battlefield+High+School,+15000+Graduation+Dr,+Haymarket,+VA+20169/@38.8450007,-77.6304667,17z/data=!4m2!3m1!1s0x89b668be1850b531:0x54b85e4d7455f560"><i class="fa fa-map-marker"></i></a>
-               <a href="https://calendar.google.com/calendar/embed?src=dGVhbTE4ODVAZ21haWwuY29t"><i class="fa fa-calendar"></i></a>
-        </div><!-- end section -->
-
-        <div class="box animate" data-anim-type="fadeInRight" data-anim-delay="400">
-                    <a href="http://instagram.com/team1885"><i class="fa fa-facebook"></i></a>
-            <a href="https://twitter.com/team1885"><i class="fa fa-twitter"></i></a>
-            <a href="https://www.facebook.com/iliterobotics/"><i class="fa fa-instagram"></i></a>
-            <a href="https://www.youtube.com/channel/UCCWs22DHGwRloB613zOJ9UA"><i class="fa fa-youtube"></i></a>
-
-        </div><!-- end section -->
-
-        <div class="box last animate" data-anim-type="fadeInRight" data-anim-delay="550">
-            <a href="index.html"><i class="fa fa-cog"></i></a>
-			       <a href="#"><i class="fa fa-code"></i></a>
-            <a href="#"><i class="fa fa-leaf"></i></a>
-            </div><!-- end section -->
-
-    </div>
-    </div><!-- end featured section 82 -->
-
-
-
-
-    <div class="featured_section208 three">
-    <div class="ctmidarea ovfull_container">
-
-        1500 Graduation Drive, Haymarket, VA 20169 <span>© 2016 ILITE Robotics. All rights reserved.</span>
-
-
-    </div>
-    </div><!-- end featured section 208 -->
-
-
-</div><!-- end contact section -->
+<?php include "../includes/footer.php" ?>
 
 <a href="#" class="scrollup">Scroll</a><!-- end scroll to top of the page-->
 
