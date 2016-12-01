96af86346d1ee3f5d943a6434750dc66943c2680
annapNC17 <n.c.annap@gmail.com>
2016-12-01 15:35:01 -0500

    Add files via upload

---
+++ FTC/README.md
@@ -0,0 +1,15 @@
+## How to Update Interior Pages:
+
+#### Add Sub header
+Add text to `<div class="head-image__subheader"></div>`
+
+#### Add Background Image to header
+Add `style="background-image: url( YOUR FILE PATH HERE )"` to `<div class="head-image inner-page">`. It should end up looking similar to: `<div class="head-image inner-page" style="background-image: url( /images/headers/mission-header.jpg )">`
+
+# Style guides
+
+### LESS style guide
+[ilite style guide] (https://github.com/GailDrake/ilite/wiki/Less-Style-guide)
+
+### General style guide
+[Meteor Style guide] (https://github.com/meteor/meteor/wiki/Meteor-Style-Guide) - Great general guidelines. We should par down at some point.
---
+++ FTC/side-menu.php
@@ -0,0 +1,35 @@
+<nav id='side-menu' class='menu width-0'>
+	<div class="nav-container">
+
+    <div id="side-menu__about" class="nav__link">
+			About <div class="caret"></div>
+			<div class="nav__link__wrapper" id="side-menu__about__wrapper">
+				<a href='about/about.php' class="nav__link__target">Event Information</a>
+				<a href='about/schedule.php' class="nav__link__target">Schedule</a>
+			</div>
+		</div>
+
+    <div id="side-menu__first" class="nav__link">
+			VOLUNTEER<div class="caret"></div>
+			<div class="nav__link__wrapper" id="side-menu__first__wrapper">
+				<a href='https://my.usfirst.org/FIRSTPortal/Login/VIMS_Login.aspx' class="nav__link__target">Volunteer</a>
+			</div>
+		</div>
+
+    <div id="side-menu__community" class="nav__link">
+			GENERAL <div class="caret"></div>
+			<div class="nav__link__wrapper" id="side-menu__community__wrapper">
+				<a href='general/maps.php' class="nav__link__target">Maps</a>
+				<a href='general/lunch.php' class="nav__link__target">Lunch</a>
+			</div>
+		</div>
+
+ 
+
+    <div id="side-menu__involved" class="nav__link action">
+			CONTACT<div class="caret"></div>
+			<div class="nav__link__wrapper" id="side-menu__involved__wrapper">
+				<a href='contactus/contact.php' class="nav__link__target">Contact Us</a>
+			</div>
+		</div>
+</nav>
