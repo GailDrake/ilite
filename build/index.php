103597a1f815df75cb0879db5bca39f83519d76b
Dylan Fernandes <dylan.fernandes00@gmail.com>
2017-01-19 19:16:08 -0500

    Update index.php

--- build/index.php
+++ build/index.php
@@ -1,4 +1,4 @@
-﻿<!doctype html>
+<!doctype html>
 <!--[if IE 7 ]>    <html lang="en-gb" class="isie ie7 oldie no-js"> <![endif]-->
 <!--[if IE 8 ]>    <html lang="en-gb" class="isie ie8 oldie no-js"> <![endif]-->
 <!--[if IE 9 ]>    <html lang="en-gb" class="isie ie9 no-js"> <![endif]-->
@@ -10,14 +10,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="keywords" content="" />
 	<meta name="description" content="" />
-
+    
 	<link rel="shortcut icon" href="images/favicon.ico">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
    	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Dancing+Script:400,700' rel='stylesheet' type='text/css'>
-
+<link rel="stylesheet" href="//brick.a.ssl.fastly.net/Roboto:400"/>
     <!--[if lt IE 9]>
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
@@ -37,15 +37,74 @@
 
 <div id="wrap" class="home__top-style right-0">
 
-	<?php include "includes/header.php" ?>
+	<nav id="navigation" class="navigation">
+  <a class="nav__logo" href="../index.php"></a>
+
+  <div id="burger">
+
+    <div class="nav-mobile"><div id="nav-toggle"><span></span></div></div>
+  </div>
+
+  <div class="nav-container">
+
+    <div id="nav__about" class="nav__link">
+			About <div class="caret"></div>
+			<div class="nav__link__wrapper" id="nav__about__wrapper">
+				<a href='/about/missionandvision.php' class="nav__link__target">Mission &amp; Vision</a>
+        <a href='/about/team.php' class="nav__link__target">Team</a>
+				<a href='/about/history.php' class="nav__link__target">History</a>
+				<a href='/about/press.php' class="nav__link__target">Press</a>
+				<a href='/about/contactus.php' class="nav__link__target">Contact Us</a>
+			</div>
+		</div>
+
+    <div id="nav__first" class="nav__link">
+			FIRST Robotics <div class="caret"></div>
+			<div class="nav__link__wrapper" id="nav__first__wrapper">
+				<a href='/first/whatisfirst.php' class="nav__link__target">About FIRST</a>
+				<a href='http://www.firstinspires.org/scholarships' class="nav__link__target">Scholarships</a>
+				<a href='/first/firstcampaign.php' class="nav__link__target">FIRST Outreach</a>
+        <a href='/first/season.php' class="nav__link__target">FIRST 2016 Season</a>
+			</div>
+		</div>
+
+    <div id="nav__community" class="nav__link">
+			Community <div class="caret"></div>
+			<div class="nav__link__wrapper" id="nav__community__wrapper">
+				<a href='/community/sponsors.php' class="nav__link__target">Sponsors</a>
+				<a href='/community/summer-camps.php' class="nav__link__target">Summer Camps</a>
+								<a href='/community/surveys.php' class="nav__link__target">Summer Camp Surveys</a>
+				<a href='/community/outreach.php' class="nav__link__target">Outreach</a>
+				<a href='/community/us-stem-foundation.php' class="nav__link__target">US STEM Foundation</a>
+				<a href='/community/hosted.php' class="nav__link__target">Hosted Events</a> 
+			</div>
+		</div>
+
+    <a href='/media/media.php' class="nav__link no-caret">Media</a>
+
+    <div id="nav__involved" class="nav__link action">
+			Get Involved <div class="caret"></div>
+			<div class="nav__link__wrapper" id="nav__involved__wrapper">
+				<a href='/getinvolved/whereisilite.php' class="nav__link__target">Where is Team 1885</a>
+				<a href='/getinvolved/volunteereventportal.php' class="nav__link__target">Volunteer Portal</a>
+				<a href='/getinvolved/studenteventportal.php' class="nav__link__target">Student Event Portal</a>
+				<a href='/getinvolved/becomeasponsor.php' class="nav__link__target">Become a Sponsor</a>
+				<a href='/getinvolved/becomeamember.php' class="nav__link__target">Become a Member</a>
+				<a href='/getinvolved/becomeamentor.php' class="nav__link__target">Become a Mentor</a>
+				<a href='/getinvolved/firstfuel.php' class="nav__link__target">Help Feed the Team</a>
+			</div>
+		</div>
+
+  </div>
+</nav>
 
 <div class="home__wrap" >
 
 	<div id="home__banner" class="home__banner">
 
 		<div class="home__banner__text-container">
-			<h1>ilite robotics</h1>
-			<h2>We are FRC Team 1885</h2>
+			<h1>We are ilite robotics.</h1>
+			<h2>FIRST Robotics Challenge Team 1885</h2>
 			<h3>We are a team founded on the principles of leadership, teamwork, and academic rigor. Through competitions, outreach, and mentorship we passionately advocate for science and technology in our schools, communities, and throughout the world.</h3>
 		</div>
 <!--
@@ -56,9 +115,8 @@
 	<div class="less5 centf bigtfont gray">Whether it be through our summer camps that teach hundreds of students about STEM, or our annually-hosted FIRST competitions, ILITE continuously inspires those around them. We strive to embody the FIRST mission of "More than Robots" in all we do."</div>
  -->
 
-    <a href="about/missionandvision.php" class="home__banner__button">Our Mission &amp; Vision</a>
-
-
+<div class="center-button">
+<a href="/about/missionandvision.php" class="button thhthree">LEARN MORE ABOUT OUR MISSION AND VISION!</a> </div>
 </div><!-- end section -->
 
 <div id="wrap5">
@@ -66,54 +124,55 @@
 	<div class="featured_section142 A">
 		<div class="container">
 
-			<p class="home__section-header">upcoming events</p>
+			<p class="home__section-header">fall events</p>
 			<p class="home__section-sub-header">Come support Team 1885 and our STEM leaders at work in the community.</p>
 
   		<div class="clearfix margin_top3"></div>
 
-			<div class="one_third animate" data-anim-type="fadeInUp" data-anim-delay="200">
-
-				<div class="home__img-holder">
-<div class="home__img summer-camp"></div>
-
-				</div>
- <div class="addes">
-		      <em><a href="https://www.google.com/maps/dir/''/battlefield+high+school/@38.8449798,-77.7005067,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89b668be1850b531:0x54b85e4d7455f560!2m2!1d-77.6304667!2d38.8450007"><i class="fa fa-map-marker"></i> Google Map</a></em>
-		      <strong><a href="community/summer-camps.php">2016 Summer Camps</a></strong>
-					<strong>June 21st-24th, June 27th-30th</strong>
-					15000 Graduation Dr, Haymarket, VA 20169
-				</div>
-
-
-
-
-			</div><!-- end section -->
-
 		  <div class="one_third animate" data-anim-type="fadeInUp" data-anim-delay="250">
 
 				<div class="home__img-holder">
-					<div class="home__img first-stronghold"></div>
+					<div class="home__img e1"></div>
 				</div>
 
 		  <div class="addes">
 		      <em><a href="https://www.google.com/maps/dir/''/battlefield+high+school/@38.8449798,-77.7005067,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89b668be1850b531:0x54b85e4d7455f560!2m2!1d-77.6304667!2d38.8450007"><i class="fa fa-map-marker"></i> Google Map</a></em>
-		      <strong>RoboProm, ILITE Hosted</strong>
-					<strong>March 5th @ 9:00 pm - 10:00 pm</strong>
+		      <strong><a href="www.irocoffseason.org">FTC Robotics NoVA Regional</a></strong>
+					<strong>December 10th, 2016</strong>
 					15000 Graduation Dr, Haymarket, VA 20169
 				</div>
 
 			</div><!-- end section -->
+			
+			<div class="one_third animate" data-anim-type="fadeInUp" data-anim-delay="200">
+
+				<div class="home__img-holder">
+<div class="home__img e2"></div>
+				
+				</div>
+ <div class="addes">
+		      <em><a href="https://www.google.com/maps/dir/''/battlefield+high+school/@38.8449798,-77.7005067,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89b668be1850b531:0x54b85e4d7455f560!2m2!1d-77.6304667!2d38.8450007"><i class="fa fa-map-marker"></i> Google Map</a></em>
+		      <strong><a href="community/hosted.php#ftc">3rd Annual HayMaker STEM Expo</a></strong>
+					<strong>January 28th, 2017</strong>
+					15000 Graduation Dr, Haymarket, VA 20169
+				</div>
+
+
+		  
+
+			</div><!-- end section -->
+
 
 		  <div class="one_third last animate" data-anim-type="fadeInUp" data-anim-delay="300">
 
 				<div class="home__img-holder">
-					<div class="home__img first"></div>
+					<div class="home__img e3"></div>
 				</div>
 
 		    <div class="addes">
 		      <em><a href="https://www.google.com/maps/dir/''/battlefield+high+school/@38.8449798,-77.7005067,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89b668be1850b531:0x54b85e4d7455f560!2m2!1d-77.6304667!2d38.8450007"><i class="fa fa-map-marker"></i> Google Map</a></em>
-		      <strong>FRC Districts, ILITE Hosted</strong>
-					<strong>March 5th - March 6th</strong>
+		 	      <strong><a href="community/hosted.php#frc">FRC District Event</a></strong>
+			<strong>March 4th and 5th, 2017</strong>
 					15000 Graduation Dr, Haymarket, VA 20169
 				</div>
 
@@ -127,7 +186,7 @@
 			<p class="home__section-header white">news</p>
 			<p class="home__section-sub-header white">
 				Stay up to date with the competitions, exhibitions, and educational outreach
-				<span class="desktop-break">intitiatives that ILITE frequently hosts and participates in.</span>
+				<span class="desktop-break">initiatives that ILITE frequently hosts and participates in.</span>
 			</p>
 
 			<div class="clearfix margin_top3"></div>
@@ -136,37 +195,37 @@
 
 				<div class="one_third animate" data-anim-type="fadeInUp" data-anim-delay="200">
 					<a class="home__img-holder" href="#">
-						<div class="home__img champs-2015"></div>
+						<div class="home__img n3"></div> 
 					</a>
 					<div class="addes">
-						<strong>ILITE @ World Championships 2016</strong>
-						ILITE does it again! FRC Team 1885, ILITE qualified for the World Championships, a <i>12 year streak</i>, after winning it at the district and regional levels. ILITE has also won the Entrepreneurship and the Industrial Design regional Awards.
-					</div>
-
-
+						<strong>Updated STEM Expo and FIRST Fuel Sign-Ups</strong>
+						Team-members and parents should be aware of updated <a href="http://www7.mysignup.com/cgi-bin/view.cgi?datafile=fuel2017">FIRST Fuel</a> (see also the <a href="/getinvolved/firstfuel.php">FIRST Fuel page</a>) and <a href="http://www.signupgenius.com/go/30e084faea929a0fc1-haymaker2">STEM Expo Sign-Up</a> forms. We thank you for your continued support of ILITE!</div>
+					
+				
 				</div><!-- end section -->
 
 				<div class="one_third animate" data-anim-type="fadeInUp" data-anim-delay="250">
-					<a class="home__img-holder" href="http://www.northernvatimes.com/gainesville/article/battlefield-hosts-stem-expo-jan.-23">
-						<div class="home__img haymaker"></div>
+					<a class="home__img-holder" href="#">
+						<div class="home__img n1"></div> 
 					</a>
 						<div class="addes">
-						<strong>ILITE Hosts HayMaker STEM Expo:</strong>
-						In the midst of build season, ILITE hosted its 2nd annual STEM Expo. The event boasted 50+ experiments, exhibits, and family-friendly activities as well as over 2000 attendees exhibits and the county's largest college fair.
-					</div>
-
-
+						<strong>2016 IROC Competition is Largest Ever Held</strong>
+						With 33 registered teams, the 2016 ILITE Robotics Off-Season Challenge was the largest ever held. The winning alliance was composed of FRC Teams 384, Sparky Robotics, 5338, RoboLoCos, and 2363, Triple Helix. Congratulations teams!</div>
+					
+				
 				</div><!-- end section -->
 
 				<div class="one_third last animate" data-anim-type="fadeInUp" data-anim-delay="300">
 
-					<a class="home__img-holder" href="http://patch.com/virginia/manassas/frc-robotics-teams-compete-ilite-robotics-season-challenge-iroc-battlefield-high-school-haymarket-va">
-						<div class="home__img hosts-iroc"></div>
+					<a class="home__img-holder" href="#">
+						<div class="home__img n2"></div> 
 					</a>
 <div class="addes">
-						<strong>ILITE Hosts &amp; Shines at IROC:</strong>
-						IROC, ILITE Robotics Offseason Challenge, is a FIRST robotics off season hosted annually by Battlefield High School FRC Team 1885, ILITE Robotics in Haymarket. This year's game was the 2015 FRC Challenge, Recycle Rush.
-					</div>
+						<strong>ILITE Chosen To Be Kindness Ambassadors</strong>
+						ILITE has been chosen to be kindness ambassadors for our school and community. Recent developments include enacting kindness rules at our high school, starting a kindness week at out school, and spreading kindess at the PWCS Walk for Autism.</div>
+					
+					
+					
 				</div><!-- end section -->
 			</div>
 
@@ -245,6 +304,10 @@
 					<div style="background-image: url( images/sponsors/16.png )" class="home__sponsors__logo"></div>
 				</a>
 
+				<a href="https://www.army.mil/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/6.jpg )" class="home__sponsors__logo"></div>
+				</a>
+
 				<a href="http://www.datasynctech.com/" class="home__sponsors__logo-link" target="_blank">
 					<div style="background-image: url( images/sponsors/5.png )" class="home__sponsors__logo"></div>
 				</a>
@@ -269,17 +332,122 @@
 <div class="clearfix"></div>
 
 
-<?php include "includes/footer.php" ?>
+<div class="footer">
+  <div class="footerInfo">
+
+    <div class="col" >
+      <h5 class="footer__header">CONTACT US</h5>
+      <a class="footer__link" href="mailto:gddrake@gmail.com" >drakegd@pwcs.edu</a>
+      <p class="footer__text">Coach Gail Drake</p>
+      <p class="footer__text">(571)261-4726</p>
+    </div>
+
+    <div class="col" >
+      <h5 class="footer__header">LOCATION</h5>
+      <a class="footer__link" href="/about/contactus.php" >
+        <i class="fa fa-map-marker"></i>
+        Map &amp; Directions
+      </a>
+      <p class="footer__text">ILITE Robotics</p>
+      <p class="footer__text">15000 Graduation Drive</p>
+      <p class="footer__text">Haymarket, VA 20169</p>
+    </div>
+
+    <div class="col" >
+      <h5 class="footer__header">CALENDAR</h5>
+      <a class="footer__link" href="/getinvolved/whereisilite.php">
+        <i class="fa fa-calendar-o"></i>
+        Team Events &amp; Schedule
+      </a>
+    </div>
+
+    <div class="col" >
+      <h5 class="footer__header">GET CONNECTED</h5>
+      <a href="https://www.facebook.com/iliterobotics/" target="_blank" class="footer__social">
+        <i class="fa fa-facebook"></i>
+      </a>
+      <a href="https://www.twitter.com/team1885" target="_blank" class="footer__social">
+        <i class="fa fa-twitter"></i>
+      </a>
+      <a href="https://www.youtube.com/channel/UCCWs22DHGwRloB613zOJ9UA" target="_blank" class="footer__social">
+        <i class="fa fa-youtube"></i>
+      </a>
+      <a href="https://www.instagram.com/team1885" target="_blank" class="footer__social">
+        <i class="fa fa-instagram"></i>
+      </a>
+    </div>
+  </div>
+
+
+</div>
+
+<div class="footer__copyright">
+  <p>&copy; <span id="copyright-year"></span> ILITE Robotics. All rights reserved.</p>
+</div>
 
 
 <a href="#" class="scrollup">Scroll</a><!-- end scroll to top of the page-->
 
 </div>
 
-<?php include "includes/side-menu.php" ?>
+<nav id='side-menu' class='menu width-0'>
+	<div class="nav-container">
+
+    <div id="side-menu__about" class="nav__link">
+			About <div class="caret"></div>
+			<div class="nav__link__wrapper" id="side-menu__about__wrapper">
+				<a href='/about/missionandvision.php' class="nav__link__target">Mission &amp; Vision</a>
+        <a href='/about/team.php' class="nav__link__target">Team</a>
+				<a href='/about/history.php' class="nav__link__target">History</a>
+				<a href='/about/press.php' class="nav__link__target">Press</a>
+				<a href='/about/contactus.php' class="nav__link__target">Contact Us</a>
+			</div>
+		</div>
+
+    <div id="side-menu__first" class="nav__link">
+			FIRST Robotics <div class="caret"></div>
+			<div class="nav__link__wrapper" id="side-menu__first__wrapper">
+				<a href='/first/whatisfirst.php' class="nav__link__target">About FIRST</a>
+				<a href='/first/scholarships.php' class="nav__link__target">Scholarships</a>
+				<a href='/first/firstcampaign.php' class="nav__link__target">FIRST Outreach</a>
+        <a href='/first/season.php' class="nav__link__target">FIRST 2016 Season</a>
+			</div>
+		</div>
+
+    <div id="side-menu__community" class="nav__link">
+			Community <div class="caret"></div>
+			<div class="nav__link__wrapper" id="side-menu__community__wrapper">
+				<a href='/community/sponsors.php' class="nav__link__target">Sponsors</a>
+				<a href='/community/summer-camps.php' class="nav__link__target">Summer Camps</a>
+								<a href='/community/surveys.php' class="nav__link__target">Summer Camps Surveys</a>
+				<a href='/community/outreach.php' class="nav__link__target">Outreach</a>
+				<a href='/community/us-stem-foundation.php' class="nav__link__target">US STEM Foundation</a>
+				<!-- <a href='/community/hosted-events.php' class="nav__link__target">Hosted Events</a> -->
+			</div>
+		</div>
+
+    <a href='/media/media.php' class="nav__link no-caret">Media</a>
+
+    <div id="side-menu__involved" class="nav__link action">
+			Get Involved <div class="caret"></div>
+			<div class="nav__link__wrapper" id="side-menu__involved__wrapper">
+				<a href='/getinvolved/whereisilite.php' class="nav__link__target">Where is Team 1885</a>
+				<a href='/getinvolved/volunteereventportal.php' class="nav__link__target">Volunteer Portal</a>
+				<a href='/getinvolved/studenteventportal.php' class="nav__link__target">Student Event Portal</a>
+				<a href='/getinvolved/becomeasponsor.php' class="nav__link__target">Become a Sponsor</a>
+				<a href='/getinvolved/becomeamember.php' class="nav__link__target">Become a Member</a>
+				<a href='/getinvolved/becomeamentor.php' class="nav__link__target">Become a Mentor</a>
+				<a href='/getinvolved/firstfuel.php' class="nav__link__target">Help Feed the Team</a>
+			</div>
+		</div>
+</nav>
 
 <!-- ######### JS FILES ######### -->
-<?php include "includes/javascript.php" ?>
+<!-- get jQuery -->
+<script type="text/javascript" src="/js/universal/jquery.js"></script>
+
+<!-- Whole site JS -->
+<script src="/js/ilite/min/ilite.min.js" type="text/javascript"></script>
 
 <!-- Home page only JS -->
 <script src="js/home/min/home.min.js" type="text/javascript"></script>
@@ -288,7 +456,7 @@
 
 <script src="js/onepage3/bootstrap.min.js"></script>
 <script src="js/onepage3/jPinning.js"></script>
-
+<script type="text/javascript" src="js/home/button.js"></script>
 <script src="js/scrolltotop/totop.js" type="text/javascript"></script>
 <script type="text/javascript" src="js/mainmenu/modernizr.custom.75180.js"></script>
 
