c6dccac0ce50a0197641d7122aa5087a736cd78d
urvi-pisal <urvipisal@gmail.com>
2017-09-14 18:30:45 -0400

    Fixed breaks in links

--- build/getinvolved/studenteventportal.php
+++ build/getinvolved/studenteventportal.php
@@ -86,8 +86,8 @@
 <td>September 23, 2017</td>
 <td>Girls In Aviation</td>
 <td><span style="font-weight: 400;">This informative workshops will feature student and alumni-led sessions on the basic programming language "basic", and learn the basics of robotics. </span></td>
-<td><a href ="https://www.wai.org/giad"target="_blank">Manassas Regional Airport</a><br></br><a href= "https://www.google.com/maps/place/Manassas+Regional+Airport/@38.7255746,-77.5145078,17z/data=!3m1!4b1!4m5!3m4!1s0x89b65e87e3d15897:0x5305166bad6cb4c9!8m2!3d38.7255746!4d-77.5123191 target="_blank"">10600 Harry J Parrish Blvd, Manassas, VA 20110</a></td>
-<td><strong>No sign up sheet yet</strong></td>
+<td><a href ="https://www.wai.org/giad"target="_blank">Manassas Regional Airport</a><br></br><a href= "https://www.google.com/maps/place/Manassas+Regional+Airport/@38.7255746,-77.5145078,17z/data=!3m1!4b1!4m5!3m4!1s0x89b65e87e3d15897:0x5305166bad6cb4c9!8m2!3d38.7255746!4d-77.5123191 target="_blank"">10600 Harry J Parrish Blvd <br> Manassas, VA 20110</a></td>
+<td><strong>None</strong></td>
 </tr>
 </thead>
 </table>
@@ -111,35 +111,35 @@
 	<td>September 23, 2017</td>
 	<td>Girls In Aviation</td>
 	<td><span style="font-weight: 400;">This informative workshops will feature student and alumni-led sessions on the basic programming language "basic", and learn the basics of robotics. </span></td>
-	<td><a href ="https://www.wai.org/giad"target="_blank">Manassas Regional Airport</a><br></br><a href= "https://www.google.com/maps/place/Manassas+Regional+Airport/@38.7255746,-77.5145078,17z/data=!3m1!4b1!4m5!3m4!1s0x89b65e87e3d15897:0x5305166bad6cb4c9!8m2!3d38.7255746!4d-77.5123191 target="_blank"">10600 Harry J Parrish Blvd, Manassas, VA 20110</a></td>
-	<td><strong>No sign up sheet yet</strong></td>
+	<td><a href ="https://www.wai.org/giad"target="_blank">Manassas Regional Airport</a><br></br><a href= "https://www.google.com/maps/place/Manassas+Regional+Airport/@38.7255746,-77.5145078,17z/data=!3m1!4b1!4m5!3m4!1s0x89b65e87e3d15897:0x5305166bad6cb4c9!8m2!3d38.7255746!4d-77.5123191 target="_blank"">10600 Harry J Parrish Blvd<br> Manassas, VA 20110</a></td>
+	<td><strong>None</strong></td>
 	</tr>
 <tr>
 <td>October 15, 2017</td>
 <td>Payton's Walk</td>
 <td><span style="font-weight: 400;">Join our walk in support of the Payton Project,and freeze bullying. Run or walk while enjoying live music, facepainting, refreshments, and much more!</span></td>
-<td><a href="http://paytonsproject.org/"target="_blank">Marris Pavillion </a><br></br><a href= "https://www.google.com/maps/place/Morris+Pavillion/@36.7777774,-103.9680488,4z/data=!4m8!1m2!2m1!1sMarris+Pavilion!3m4!1s0x8859f6dd77fba7b5:0x3923579b7f3f1955!8m2!3d35.6119634!4d-82.4401103"target="_blank">East Ln, Swannanoa, NC 28778</a></td>
-<td><strong>No sign up sheet yet</strong></td>
+<td><a href="http://paytonsproject.org/"target="_blank">Marris Pavillion </a><br></br><a href= "https://www.google.com/maps/place/Morris+Pavillion/@36.7777774,-103.9680488,4z/data=!4m8!1m2!2m1!1sMarris+Pavilion!3m4!1s0x8859f6dd77fba7b5:0x3923579b7f3f1955!8m2!3d35.6119634!4d-82.4401103"target="_blank">East Ln<br> Swannanoa, NC 28778</a></td>
+<td><strong>Coming up soon</strong></td>
 </tr>
 <tr>
 <td>October 21, 2017</td>
 <td>IROC Competition</td>
 <td>ILITE (Inspiring Leaders in Technology and Engineering) Robotics is hosting their off-season robotics competition again this year. IROC, the ILITE Robotics Off-Season Challenge, features the previous year’s FRC (FIRST Robotics Competition) game. The 2017 game, FIRST Steamworks, is a steampunk-themed game in which robots are built and programmed by students to score goals on the airship, in addition to scoring balls in 12ft towers.</td>
-<td><a href="https://battlefieldhs.pwcs.edu/"target="_blank">Battlefield Highschool</a><br></br><a href="https://www.google.com/maps/place/Battlefield+High+School/@38.8453368,-77.6319955,17z/data=!3m1!4b1!4m5!3m4!1s0x89b668be1850b531:0x54b85e4d7455f560!8m2!3d38.8453326!4d-77.6298068"target="_blank">15000 Graduation Dr, Haymarket, VA 20169</a></td>
+<td><a href="https://battlefieldhs.pwcs.edu/"target="_blank">Battlefield Highschool</a><br></br><a href="https://www.google.com/maps/place/Battlefield+High+School/@38.8453368,-77.6319955,17z/data=!3m1!4b1!4m5!3m4!1s0x89b668be1850b531:0x54b85e4d7455f560!8m2!3d38.8453326!4d-77.6298068"target="_blank">15000 Graduation Dr<br> Haymarket, VA 20169</a></td>
 <td><a href="http://irocoffseason.org/registervolunteer/volunteer.php">Sign Up</a></td>
 </tr>
 <tr>
 <td>October 28, 2017</td>
 <td>Air and Scare</td>
 <td>Explore the spooky side of air and space at Air and Scare, our annual Halloween event at the Steven F. Udvar-Hazy Center in Chantilly, Va. Participate in creepy crafts, spooky science experiments, and other Halloween-themed activities. Arrive in costume to get into the Halloween spirit and enjoy safe indoor trick-or-treating.</td>
-<td><a href= "https://airandspace.si.edu/udvar-hazy-center"target="_blank">Udvar-Hazy Center</a><br></br><a href="https://www.google.com/maps/place/Steven+F.+Udvar-Hazy+Center/@38.9109231,-77.4463962,17z/data=!3m1!4b1!4m5!3m4!1s0x89b646eb32642073:0xb15e71e7680ccaa8!8m2!3d38.9109189!4d-77.4442075"target="_blank">14390 Air and Space Museum Pkwy, Chantilly, VA 20151</a></td>
+<td><a href= "https://airandspace.si.edu/udvar-hazy-center"target="_blank">Udvar-Hazy Center</a><br></br><a href="https://www.google.com/maps/place/Steven+F.+Udvar-Hazy+Center/@38.9109231,-77.4463962,17z/data=!3m1!4b1!4m5!3m4!1s0x89b646eb32642073:0xb15e71e7680ccaa8!8m2!3d38.9109189!4d-77.4442075"target="_blank">14390 Air and Space Museum Pkwy<br> Chantilly, VA 20151</a></td>
 <td><strong>No sign up sheet yet</strong></td>
 </tr>
 <tr>
 <td>January 27, 2018</td>
 <td>HayMaker STEAM Expo</td>
 <td>ILITE Robotics will host its 4th annual STEM and Robotics Expo. Numerous kid friendly activities will be offered including a Maker Fare sponsored in part by local businesses, universities, and organizations.</td>
-<td><a href="https://battlefieldhs.pwcs.edu/"target="_blank">Battlefield Highschool</a><br></br><a href="https://www.google.com/maps/place/Battlefield+High+School/@38.8453368,-77.6319955,17z/data=!3m1!4b1!4m5!3m4!1s0x89b668be1850b531:0x54b85e4d7455f560!8m2!3d38.8453326!4d-77.6298068"target="_blank">15000 Graduation Dr, Haymarket, VA 20169</a></td>
+<td><a href="https://battlefieldhs.pwcs.edu/"target="_blank">Battlefield Highschool</a><br></br><a href="https://www.google.com/maps/place/Battlefield+High+School/@38.8453368,-77.6319955,17z/data=!3m1!4b1!4m5!3m4!1s0x89b668be1850b531:0x54b85e4d7455f560!8m2!3d38.8453326!4d-77.6298068"target="_blank">15000 Graduation Dr<br> Haymarket, VA 20169</a></td>
 <td><strong>No sign up sheet yet</strong></td>
 </tr>
 </thead>
