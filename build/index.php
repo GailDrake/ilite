f189931a80960ecefc05afe3a9ddf2e89907de6e
Developer5262 <dylan.fernandes00@gmail.com>
2018-01-13 13:50:18 -0500

    updated home page

--- build/index.php
+++ build/index.php
@@ -45,28 +45,46 @@
             About
             <div class="caret"></div>
             <div class="nav__link__wrapper" id="nav__about__wrapper">
-              <a class="nav__link__target" href='/about/missionandvision.php'>Mission &amp; Vision</a> <a class="nav__link__target" href='/about/team.php'>Team</a> <a class="nav__link__target" href='/about/history.php'>History</a> <a class="nav__link__target" href='/about/press.php'>Press</a> <a class="nav__link__target" href='/about/contactus.php'>Contact Us</a>
+              <a class="nav__link__target" href='/about/missionandvision.php'>Mission &amp; Vision</a> 
+              <a class="nav__link__target" href='/about/team.php'>Team</a> 
+              <a class="nav__link__target" href='/about/history.php'>History</a>
+              <a class="nav__link__target" href='/about/press.php'>Press</a>
+              <a class="nav__link__target" href='/about/contactus.php'>Contact Us</a>
             </div>
           </div>
           <div class="nav__link" id="nav__first">
             FIRST Robotics
             <div class="caret"></div>
             <div class="nav__link__wrapper" id="nav__first__wrapper">
-              <a class="nav__link__target" href='/first/whatisfirst.php'>About FIRST</a> <a class="nav__link__target" href='http://www.firstinspires.org/scholarships'>Scholarships</a> <a class="nav__link__target" href='/first/firstcampaign.php'>FIRST Outreach</a> <a class="nav__link__target" href='/first/season.php'>FIRST 2016 Season</a>
+              <a class="nav__link__target" href='/first/whatisfirst.php'>About FIRST</a>
+              <a class="nav__link__target" href='http://www.firstinspires.org/scholarships'>Scholarships</a>
+              <a class="nav__link__target" href='/first/firstcampaign.php'>FIRST Outreach</a>
+              <a class="nav__link__target" href='/first/season.php'>FIRST 2016 Season</a>
             </div>
           </div>
           <div class="nav__link" id="nav__community">
             Community
             <div class="caret"></div>
             <div class="nav__link__wrapper" id="nav__community__wrapper">
-              <a class="nav__link__target" href='/community/sponsors.php'>Sponsors</a> <a class="nav__link__target" href='/community/summer-camps.php'>Summer Camps</a> <a class="nav__link__target" href='/community/surveys.php'>Summer Camp Surveys</a> <a class="nav__link__target" href='/community/outreach.php'>Outreach</a> <a class="nav__link__target" href='/community/us-stem-foundation.php'>US STEM Foundation</a> <a class="nav__link__target" href='/community/hosted.php'>Hosted Events</a>
+              <a class="nav__link__target" href='/community/sponsors.php'>Sponsors</a>
+              <a class="nav__link__target" href='/community/summer-camps.php'>Summer Camps</a>
+              <a class="nav__link__target" href='/community/surveys.php'>Summer Camp Surveys</a>
+              <a class="nav__link__target" href='/community/outreach.php'>Outreach</a>
+              <a class="nav__link__target" href='/community/us-stem-foundation.php'>US STEM Foundation</a>
+              <a class="nav__link__target" href='/community/hosted.php'>Hosted Events</a>
             </div>
           </div><a class="nav__link no-caret" href='/media/media.php'>Media</a>
           <div class="nav__link action" id="nav__involved">
             Get Involved
             <div class="caret"></div>
             <div class="nav__link__wrapper" id="nav__involved__wrapper">
-              <a class="nav__link__target" href='/getinvolved/whereisilite.php'>Where is Team 1885</a> <a class="nav__link__target" href='/getinvolved/volunteereventportal.php'>Volunteer Portal</a> <a class="nav__link__target" href='/getinvolved/studenteventportal.php'>Student Event Portal</a> <a class="nav__link__target" href='/getinvolved/becomeasponsor.php'>Become a Sponsor</a> <a class="nav__link__target" href='/getinvolved/becomeamember.php'>Become a Member</a> <a class="nav__link__target" href='/getinvolved/becomeamentor.php'>Become a Mentor</a> <a class="nav__link__target" href='/getinvolved/firstfuel.php'>Help Feed the Team</a>
+              <a class="nav__link__target" href='/getinvolved/whereisilite.php'>Where is Team 1885</a> 
+              <a class="nav__link__target" href='/getinvolved/volunteereventportal.php'>Volunteer Portal</a> 
+              <a class="nav__link__target" href='/getinvolved/studenteventportal.php'>Student Event Portal</a> 
+              <a class="nav__link__target" href='/getinvolved/becomeasponsor.php'>Become a Sponsor</a> 
+              <a class="nav__link__target" href='/getinvolved/becomeamember.php'>Become a Member</a> 
+              <a class="nav__link__target" href='/getinvolved/becomeamentor.php'>Become a Mentor</a> 
+              <a class="nav__link__target" href='/getinvolved/firstfuel.php'>Help Feed the Team</a>
             </div>
           </div>
         </div>
@@ -76,14 +94,13 @@
           <div class="home__banner__text-container">
             <h1>We are ilite robotics.</h1>
             <h2>FIRST Robotics Challenge Team 1885</h2>
-            <h3>We are a team founded on the principles of leadership, teamwork, and academic rigor. Through competitions, outreach, and mentorship we passionately advocate for science and technology in our schools, communities, and throughout the world.</h3>
-          </div><!--
-    <div class="less5 centf bigtfont gray">Here at ILITE First Team 1885, we believe that every student is a leader. Through our different subteams of business, graphics, electronics, programming, and build, we work very hard to give every team member the opportunity to learn and lead. We have created an environment in which FIRST Robotics is so loved that our alumni volunteer across the nation even after graduating. </div><br>
-
-    <div class="less5 centf bigtfont gray">Our leadership reaches beyond of FIRST Team 1885. We have created an identity for ourselves in our community, county, state, and nation. ILITE reaches out to our community through town festivals, charitable involvement, elementary school tutoring, and hosting our annual Haymaker STEM Expo. On a broader scale, ILITE members have lobbied politicians through their School Board and meetings at the Capitol to encourage the advancement of STEM in education. </div><br>
-
-    <div class="less5 centf bigtfont gray">Whether it be through our summer camps that teach hundreds of students about STEM, or our annually-hosted FIRST competitions, ILITE continuously inspires those around them. We strive to embody the FIRST mission of "More than Robots" in all we do."</div>
- -->
+            <h3>
+              We are a team founded on the principles of leadership, teamwork, and 
+              academic rigor. Through competitions, outreach, and mentorship we 
+              passionately advocate for science and technology in our schools, 
+              communities, and throughout the world.
+            </h3>
+          </div>
           <div class="center-button">
             <a class="button thhthree" href="/about/missionandvision.php">LEARN MORE ABOUT OUR MISSION AND VISION!</a>
           </div>
@@ -99,7 +116,9 @@
                   <div class="home__img e1"></div>
                 </div>
                 <div class="addes">
-                  <em><a href="https://www.google.com/maps/dir/''/battlefield+high+school/@38.8449798,-77.7005067,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89b668be1850b531:0x54b85e4d7455f560!2m2!1d-77.6304667!2d38.8450007"><i class="fa fa-map-marker"></i> Google Map</a></em> <strong><a href="http://www.irocoffseason.org">FTC Robotics NoVA Regional</a></strong> <strong>December 10th, 2016</strong> 15000 Graduation Dr, Haymarket, VA 20169
+                  <em><a href="https://www.google.com/maps/dir/''/battlefield+high+school/@38.8449798,-77.7005067,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89b668be1850b531:0x54b85e4d7455f560!2m2!1d-77.6304667!2d38.8450007"><i class="fa fa-map-marker"></i> Google Map</a></em> 
+                  <strong><a href="http://www.irocoffseason.org">FTC Robotics NoVA Regional</a></strong>
+                  <strong>December 10th, 2017</strong> 15000 Graduation Dr, Haymarket, VA 20169
                 </div>
               </div><!-- end section -->
               <div class="one_third animate" data-anim-delay="200" data-anim-type="fadeInUp">
@@ -107,15 +126,19 @@
                   <div class="home__img e2"></div>
                 </div>
                 <div class="addes">
-                  <em><a href="https://www.google.com/maps/dir/''/battlefield+high+school/@38.8449798,-77.7005067,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89b668be1850b531:0x54b85e4d7455f560!2m2!1d-77.6304667!2d38.8450007"><i class="fa fa-map-marker"></i> Google Map</a></em> <strong><a href="community/hosted.php#ftc">3rd Annual HayMaker STEM Expo</a></strong> <strong>January 28th, 2017</strong> 15000 Graduation Dr, Haymarket, VA 20169
+                  <em><a href="https://www.google.com/maps/dir/''/battlefield+high+school/@38.8449798,-77.7005067,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89b668be1850b531:0x54b85e4d7455f560!2m2!1d-77.6304667!2d38.8450007"><i class="fa fa-map-marker"></i> Google Map</a></em>
+                  <strong><a href="community/hosted.php#ftc">3rd Annual HayMaker STEM Expo</a></strong>
+                  <strong>January 28th, 2018</strong> 15000 Graduation Dr, Haymarket, VA 20169
                 </div>
               </div><!-- end section -->
               <div class="one_third last animate" data-anim-delay="300" data-anim-type="fadeInUp">
                 <div class="home__img-holder">
-                  <div class="home__img e3"></div>
+                  <div class="home__img e3 logo__frcpowerup"></div>
                 </div>
                 <div class="addes">
-                  <em><a href="https://www.google.com/maps/dir/''/battlefield+high+school/@38.8449798,-77.7005067,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89b668be1850b531:0x54b85e4d7455f560!2m2!1d-77.6304667!2d38.8450007"><i class="fa fa-map-marker"></i> Google Map</a></em> <strong><a href="community/hosted.php#frc">FRC District Event</a></strong> <strong>March 4th and 5th, 2017</strong> 15000 Graduation Dr, Haymarket, VA 20169
+                  <em><a href="https://www.google.com/maps/dir/''/battlefield+high+school/@38.8449798,-77.7005067,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89b668be1850b531:0x54b85e4d7455f560!2m2!1d-77.6304667!2d38.8450007"><i class="fa fa-map-marker"></i> Google Map</a></em> 
+                  <strong><a href="community/hosted.php#frc">FRC District Event</a></strong>
+                  <strong>March 2nd to 4th, 2018</strong> 15000 Graduation Dr, Haymarket, VA 20169
                 </div>
               </div><!-- end section -->
             </div>
@@ -123,28 +146,38 @@
           <div class="B home__news-container">
             <div class="container">
               <p class="home__section-header white">news</p>
-              <p class="home__section-sub-header white">Stay up to date with the competitions, exhibitions, and educational outreach <span class="desktop-break">initiatives that ILITE frequently hosts and participates in.</span></p>
+              <p class="home__section-sub-header white">
+                Stay up to date with the competitions, exhibitions, and educational outreach 
+                <span class="desktop-break">initiatives that ILITE frequently hosts and participates in.</span>
+              </p>
               <div class="clearfix margin_top3"></div>
               <div class="flexbox-columns">
                 <div class="one_third animate" data-anim-delay="200" data-anim-type="fadeInUp">
                   <a class="home__img-holder" href="/getinvolved/firstfuel.php">
                   <div class="home__img n3"></div></a>
                   <div class="addes">
-                    <strong>FIRST Fuel Sign-Ups</strong> Team-members and parents should be aware of the <a href="/getinvolved/firstfuel.php">FIRST Fuel page</a>. We thank you for your continued support of ILITE!
+                    <strong>FIRST Fuel Sign-Ups</strong> Team-members and parents should be aware of the 
+                    <a href="/getinvolved/firstfuel.php">FIRST Fuel page</a>. We thank you for your continued 
+                    support of ILITE!
                   </div>
                 </div><!-- end section -->
                 <div class="one_third animate" data-anim-delay="250" data-anim-type="fadeInUp">
                   <a class="home__img-holder">
                   <div class="home__img n1"></div></a>
                   <div class="addes">
-                    <strong>2016 IROC Competition is Largest Ever Held</strong> With 33 registered teams, the 2016 ILITE Robotics Off-Season Challenge was the largest ever held. The winning alliance was composed of FRC Teams 384, Sparky Robotics, 5338, RoboLoCos, and 2363, Triple Helix. Congratulations teams!
+                    <strong>STEAM Expo</strong> Come join us at our annual Haymaker STEAM Expo! We have dozens of
+                    activities for kids of all ages and technology demonstrations from a variety of vendors. For more
+                    information, please visit <a href="#">INSERT LINK HERE BEFORE PUSH</a>                  
                   </div>
                 </div><!-- end section -->
                 <div class="one_third last animate" data-anim-delay="300" data-anim-type="fadeInUp">
                   <a class="home__img-holder">
                   <div class="home__img n2"></div></a>
                   <div class="addes">
-                    <strong>ILITE Chosen To Be Kindness Ambassadors</strong> ILITE has been chosen to be kindness ambassadors for our school and community. Recent developments include enacting kindness rules at our high school, starting a kindness week at out school, and spreading kindess at the PWCS Walk for Autism.
+                    <strong>ILITE Chosen To Be Kindness Ambassadors</strong> 
+                    ILITE has been chosen to be kindness ambassadors for our school and community. Recent developments 
+                    include enacting kindness rules at our high school, starting a kindness week at out school, and spreading
+                    kindess at the PWCS Walk for Autism.
                   </div>
                 </div><!-- end section -->
               </div>
@@ -243,8 +276,8 @@
         </div>
       </nav><!-- ######### JS FILES ######### -->
       <!-- get jQuery -->
-      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-<!-- Whole site JS -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js" type="text/javascript">
+      </script> <!-- Whole site JS -->
 
       <script src="/js/ilite/min/ilite.min.js" type="text/javascript">
       </script> <!-- Home page only JS -->
