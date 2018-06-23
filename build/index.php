19d7b73835c953db0a76de8a9b26f4173f982958
Nicholas Miller <nicholascmiller123@gmail.com>
2018-06-23 13:12:11 -0400

    Added summer camp slidshows to site

--- build/index.php
+++ build/index.php
@@ -101,8 +101,8 @@
                 <div class="addes">
                   <em><a href="https://www.google.com/maps/dir/''/battlefield+high+school/@38.8449798,-77.7005067,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89b668be1850b531:0x54b85e4d7455f560!2m2!1d-77.6304667!2d38.8450007">
                   <i class="fa fa-map-marker"></i> Google Map</a></em>
-                  <strong><a href="http://www.irocoffseason.org">FTC Robotics NoVA Regional</a></strong>
-                  <strong>December 2th, 2017</strong> 15000 Graduation Dr, Haymarket, VA 20169
+                  <strong><a href="community/summer-camps.php">2018 ILITE Summer Camps</a></strong>
+                  <strong>June 12th - 15th & June 18th - 22nd, 2018</strong> 15000 Graduation Dr, Haymarket, VA 20169
                 </div>
               </div><!-- end section -->
               <div class="one_third animate" data-anim-delay="200" data-anim-type="fadeInUp">
